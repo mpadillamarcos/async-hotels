82c9f308f82cb8ab87521ad00fa9e4eb582313e9
Maria Padilla <maria.pad.mar@gmail.com>
2026-07-18 15:15:16 +0200

    package refactor

--- src/main/java/mpadillamarcos/asynchotels/AsyncHotelsApplication.java
+++ src/main/java/mpadillamarcos/asynchotels/AsyncHotelsApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package mpadillamarcos.asynchotels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AsyncHotelsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AsyncHotelsApplication.class, args);
 	}
 
 }
--- src/test/java/mpadillamarcos/asynchotels/AsyncHotelsApplicationTests.java
+++ src/test/java/mpadillamarcos/asynchotels/AsyncHotelsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package mpadillamarcos.asynchotels;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AsyncHotelsApplicationTests {
 
 	@Test
 	void contextLoads() {
